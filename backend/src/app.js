28a5893bfe815ea479d08b796bef1fb8877d45e1
habilaris <habilaris4@gmail.com>
2026-09-03 18:43:07 +0500

    Project fully innitialized

---
+++ backend/src/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import apiRoutes from "./routes/index.js";
+
+const app = express();
+
+// Parse incoming JSON body data
+app.use(express.json());
+
+// Health Check route
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "OK" });
+});
+
+// API Routes
+app.use("/api/v1", apiRoutes);
+
+// Simple 404 handler for undefined routes
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+// Simple global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Something went wrong!" });
+});
+
+export default app;
